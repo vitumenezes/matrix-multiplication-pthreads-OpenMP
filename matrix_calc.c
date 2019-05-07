9972c718be4804715a921d6b97160978083d2375
Victor Gomes <victormenezez15@gmail.com>
2019-05-07 15:00:14 -0300

    Ends the creation of the random vector. Begins implementation of the calculation

--- matrix_calc.c
+++ matrix_calc.c
@@ -31,7 +31,7 @@ pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void Usage(char* prog_name);
 void *matrizCalc(void* rank);  /* Thread function */
-int i, j, m, n, x, y, count_x = 0, count_y = 0;
+int i, j, m, n, count_x = 0, count_y = 0;
 int size_matrix;
 int **a;
 int **b;
@@ -47,22 +47,26 @@ int main(int argc, char* argv[]) {
     if (argc != 3) Usage(argv[0]);
         thread_count = strtol(argv[1], NULL, 10);
     if (thread_count <= 0 || thread_count > MAX_THREADS) Usage(argv[0]);
-
     /* Get number of size_matrix */
+
     size_matrix = atoi(argv[2]);
-    printf("%d\n", size_matrix);
 
     /* Dynamic allocation for matrices */
     // Allocating lines
     a = (int**)malloc(size_matrix * sizeof(int*));
     b = (int**)malloc(size_matrix * sizeof(int*));
     c = (int**)malloc(size_matrix * sizeof(int*));
-
     // Allocating columns
     for(i = 0; i < size_matrix; i++){
         a[i] = (int*)malloc(size_matrix * sizeof(int));
         b[i] = (int*)malloc(size_matrix * sizeof(int));
         c[i] = (int*)malloc(size_matrix * sizeof(int));
+    }
+
+    /* Allocating aux array to all possible (x,y)pairs */
+    v_coordinates = (int**)malloc(size_matrix * size_matrix * sizeof(int*));
+    for (i = 0; i < size_matrix * size_matrix; i++) {
+        v_coordinates[i] = (int*)malloc(2 * sizeof(int));
     }
 
     /* Setting random values (matriz C with -1) */
@@ -71,50 +75,40 @@ int main(int argc, char* argv[]) {
             a[i][j] = i + j;
             b[i][j] = i * j;
             c[i][j] = -1;
-            printf("%d\t", c[i][j]);
         }
-        printf("linha %d \n", i);
     }
 
-    /* Dynamic allocation for aux arrays */
-    // v_coordinates = (int**)malloc(size_matrix * size_matrix * sizeof(int*));
-    // for (i = 0; i < size_matrix * size_matrix; i++) {
-    //         v_coordinates[i] = (int*)malloc(2 * sizeof(int));
-    // }
-
-    /* Random allocation of coordinate pairs in a stack */
-    // stack initialization
-    node *stack = (node*)malloc(sizeof(node));
-    if(!stack){
-        printf("Sem memória disponivel\n");
-        exit(1);
-    } else {
-        startStack(stack);
+    /* Populating v_coordinates with size_matrix * size_matrix possible pairs */
+    for (i = 0; i < size_matrix * size_matrix; i+=size_matrix) {
+        for (j = i; j < i + size_matrix; j++){
+            v_coordinates[j][0] = count_x;
+            v_coordinates[j][1] = count_y;
+            count_y++;
+        }
+        count_y = 0;
+        count_x++;
     }
 
-    /* Populating arrays with size_matrix possibilities */
-    for (i = 0; i < size_matrix; i++) {
-        for (j = 0; j < size_matrix; j++)
-            push(stack, i, j);
-    }
-
-    exibe(stack);
-
-    shuffle(stack);
-
+    /* Shuffling the array of coordinates */
     srand(time(NULL));
-    // random i and j to create pairs.
-    // iteration size_matrix * size_matrix times
-    // for (i = 0; i < size_matrix * size_matrix; i++) {
-    //         do {
-    //             // get values between 0 and size_matrix. This value is the index
-    //             // of v_lines array
-    //             x = rand() % size_matrix;
-    //             if(x){}
-    //         } while(/* condition */);
-    //
-    //         y = rand() % size_matrix; // get values between 0 and size_matrix
-    // }
+    for (i = 0; i < size_matrix * size_matrix; i++) {
+        int first = rand() % (size_matrix * size_matrix);
+        int second = rand() % (size_matrix * size_matrix);
+
+        int x = v_coordinates[first][0];
+        int y = v_coordinates[first][1];
+
+        v_coordinates[first][0] = v_coordinates[second][0];
+        v_coordinates[first][1] = v_coordinates[second][1];
+
+        v_coordinates[second][0] = x;
+        v_coordinates[second][1] = y;
+    }
+
+    printf("\n");
+    for (i = 0; i < size_matrix * size_matrix; i++) {
+        printf("%d%d\t",v_coordinates[i][0], v_coordinates[i][1]);
+    }
 
     thread_handles = malloc (thread_count * sizeof(pthread_t));
 
@@ -122,33 +116,61 @@ int main(int argc, char* argv[]) {
         pthread_create(&thread_handles[thread], NULL,
             matrizCalc, (void*) thread);
 
-        // printf("Hello from the main thread\tNumber of lines: %d\tNumber of columns: %d\n", lines, columns); //this is another thread that has no number. It is know as the main thread.
+    for (thread = 0; thread < thread_count; thread++)
+        pthread_join(thread_handles[thread], NULL);
 
-        for (thread = 0; thread < thread_count; thread++)
-            pthread_join(thread_handles[thread], NULL);
+    for (i = 0; i < size_matrix; i++) {
+        for (j = 0; j < size_matrix; j++) {
+            printf("%d\t", c[i][j]);
+        }
+        printf("\n");
+    }
 
-        free(thread_handles);
-        return 0;
+    free(thread_handles);
+    free(a);
+    free(b);
+    free(c);
+    free(v_coordinates);
 
-        free(a);
-        free(b);
-        free(c);
-        free(v_coordinates);
-    }  /* main */
+    return 0;
+}  /* main */
 
     /*------------------------------ | Thread Function | -------------------------------------*/
     void *matrizCalc(void* rank) {
         long my_rank = (long) rank;  /* Use long in case of 64-bit system */
-        bool result;
+        bool result = false;
+        int index, x, y, mult_result = 0;
         srand(time(NULL));
 
-        // do {
-        //     for (i = 1; result && i < size_matrix; i++){
-        //         result &= v_lines[i];
-        //     }
-        // } while(!result);
+        printf("\n\n");
+        do {
+            index = rand() % size_matrix * size_matrix;
 
-        printf("Hello from thread %ld of %d\n", my_rank, thread_count);
+            if (v_coordinates[index][0] != -1) {
+                //aqui tem que ter uma condição verificando se o dado
+                //está sendo acessado (exclusão)
+                if (true) {
+                    x = v_coordinates[index][0];
+                    y = v_coordinates[index][1];
+
+                    for (i = 0; i < size_matrix; i++){
+                        mult_result += a[x][i] * b[i][y];
+                        printf("%d * %d + ", a[x][i], b[i][y]);
+                    }
+                    printf("= %d\n", mult_result);
+                    printf("Calculo do elemento %d %d da matriz C = %d\n", x, y, mult_result);
+                    c[x][y] = mult_result;
+                }
+                mult_result = 0;
+                v_coordinates[index][0] = -1;
+            }
+
+            for (i = 0; !result && i < size_matrix * size_matrix; i++)
+                if(v_coordinates[i][0] != -1) result = false;
+            printf("%d\n", result);
+        } while(!result);
+
+        // printf("Hello from thread %ld of %d\n", my_rank, thread_count);
 
         return NULL;
     }  /* Hello */
